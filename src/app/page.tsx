01a4a85f61e0ac59e70aa3887c39f361c5b4626c
hcampos <hcampospro@gmail.com>
2025-02-12 19:40:52 +0100

    style(page.tsx): format code for better readability and consistency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <>Hello world</>
+    <section className="h-dvh">
+      <Image 
+        src="/cover.svg" 
+        alt="Illustration qui représente la direction artistique du site et du festival Les CuicuiteDays 2k25" 
+        fetchPriority="high" 
+        loading="eager" 
+        width={100} 
+        height={100} 
+        className="w-full h-auto"
+      />
+    </section>
   );
 }
