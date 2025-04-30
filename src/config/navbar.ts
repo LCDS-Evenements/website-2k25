1c6bad280f34f943b4c477428e1f8347dc793db6
hcampos <hcampospro@gmail.com>
2025-04-30 10:52:13 +0200

    feat(navbar.ts): uncomment the Sponsors link in the navbar configuration to make it visible in the navigation menu

--- src/config/navbar.ts
+++ src/config/navbar.ts
@@ -8,7 +8,7 @@ export const navbarLinks: NavbarLink[] = [
   { label: "Line Up", href: "/#line-up", target: "_self" },
   { label: "Horaires", href: "/#times", target: "_self" },
   { label: "Histoire", href: "/#history", target: "_self" },
-  // { label: "Sponsors", href: "/#sponsors", target: "_self" },
+  { label: "Sponsors", href: "/#sponsors", target: "_self" },
   { label: "Merchandising", href: "/merchandising", target: "_self" },
   { label: "Aftermovies", href: "/aftermovies", target: "_self" },
   // { label: "FAQ", href: "/#faq", target: "_self" },
